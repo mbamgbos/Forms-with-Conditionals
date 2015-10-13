e9a1a56875a3d1e834390a295d2ed15406b1f7b2
Margaret Bamgbose <Owner@apples-mbp.attlocal.net>
2015-10-12 23:07:23 -0400

    questions on code

--- run.rb
+++ run.rb
@@ -7,6 +7,32 @@ get '/' do
   erb :index, layout: :main
 end
 
+@states=[]
+
+  state={}
+  state={id:'IN', name:'Indiana'}
+  @states<<state
+
+  state={}
+  state={id:'IL', name:'Illinois'}
+  @states<<state
+
+  state={}
+  state={id:'OH', name:'Ohio'}
+  @states<<state
+
+  state={}
+  state={id:'MI', name:'Michigan'}
+  @states<<state
+
+  state={}
+  state={id:'IA', name:'Iowa'}
+  @states<<state
+
+  @states.sort_by! {| state | state[:name]}
+
+
+
 get '/form' do
   # code!
   erb :form, layout: :main
